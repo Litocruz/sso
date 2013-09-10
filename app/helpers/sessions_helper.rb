82acf38ff4e0c9ba4f0de1be544577e2c7193980
Julian Lamadrid <jelamadrid@e-din.com.ar>
2013-09-10 20:50:59 -0300

    agregado de redirect_back_or

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,4 +20,10 @@ module SessionsHelper
     self.current_employee = nil
     cookies.delete(:remember_token)
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
 end
