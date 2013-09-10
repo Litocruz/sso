4201efdc3a0d191f9e3ca890ddcbbc82def5beea
Julian Lamadrid <rengolamadrid@hotmail.com>
2013-09-10 15:10:53 -0300

    Seguimos con alta empleados

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,18 @@
 class EmployeesController < ApplicationController
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
   def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(params[:employee])
+    if @employee.save
+    else
+      render 'new'
+    end
   end
 end
--- app/helpers/employees_helper.rb
+++ app/helpers/employees_helper.rb
@@ -1,2 +1,9 @@
 module EmployeesHelper
+  #Returns the Gravatar (http://gravatar.com/) for the given employee
+  def gravatar_for(employee, options = {size: 80})
+    gravatar_id = Digest::MD5::hexdigest(employee.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+    image_tag(gravatar_url, alt: employee.name, class: "gravatar")
+  end
 end
