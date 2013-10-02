963c4cffeca039537f448c3e2b0f82caaecd61a8
Julian Lamadrid <jelamadrid@e-din.com.ar>
2013-10-02 20:54:20 -0300

    arreglos en validaciones y campos

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -7,7 +7,8 @@ class Employee < ActiveRecord::Base
   accepts_nested_attributes_for :driver_licenses, :reject_if => lambda { |a| a[:code].blank? }, :allow_destroy => true
   accepts_nested_attributes_for :studies, :reject_if => lambda { |a| a[:title].blank? }, :allow_destroy => true
   
-
+  DOCUMENTS = ["Dni", "Lc", "Le", "Pasaporte"]
+  CATEGORIES = ["B1", "B2", "B3", "Pasaporte"]
 
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -15,11 +16,15 @@ class Employee < ActiveRecord::Base
     format: { with: VALID_EMAIL_REGEX},
     uniqueness: { case_sensitive: false }
   validates :document_type, presence: true
-  validates :document_number, presence: true, uniqueness: true
+  validates :document_number, presence: true, uniqueness: true, numericality: true
   default_scope {where(status: true)}
 
   def gender_txt
     ["Hombre", "Mujer"][self.sex]
+  end
+
+  def document_txt
+    ["Dni", "Lc", "Le", "Pasaporte"][self.document_type]
   end
 
   def self.search(search)
