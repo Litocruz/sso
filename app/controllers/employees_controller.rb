073c0a79837d227a1421c8e854c3d5fecfc1141a
Julian Lamadrid <rengolamadrid@hotmail.com>
2013-10-03 15:39:28 -0300

    comenzando a agregar campos dianmicamente

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -73,16 +73,16 @@ class EmployeesController < ApplicationController
       flash[:success] = "Empleado dado de baja"
       #redirect_back_or employees_path
       @employees = Employee.order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 15)
-
+      #redirect_to employees_path
+=begin
       respond_to do |format|
         format.html { redirect_to employees_path }
         format.js 
         format.json
       end
 
-    #redirect_to employees_path
-
-    else
+    
+   else
       flash[:error]="No se pudo dar de baja el empleado"
       respond_to do |format|
         #render 'index'
@@ -90,6 +90,7 @@ class EmployeesController < ApplicationController
         format.js 
         format.json 
       end
+=end
     end
     #redirect_back_or employees_path
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+
+  def link_to_add_fields(name, f, association)  
+    new_object = f.object.class.reflect_on_association(association).klass.new  
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |b|  
+      render(association.to_s.singularize + "_fields", :f => b)  
+    end  
+    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))  
+  end 
+
   #Si no encuentra un subtitulo agrega
   #uno por defecto
   def full_title (page_title)
