7dbbe9a2144a472181ff58aeaa043150c40d1ccc
Julian Lamadrid <rengolamadrid@hotmail.com>
2013-10-06 21:41:03 -0300

    eliminar migraciones fallidas

--- db/migrate/20131004150016_change_driver_license_code_id_for_code_on_driver_license.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDriverLicenseCodeIdForCodeOnDriverLicense < ActiveRecord::Migration
-  def up
-    rename_column :driver_licenses, :code, :driver_license_code_id
-  end
-
-  def down
-  end
-end
