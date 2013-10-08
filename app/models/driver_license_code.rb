accd3709b7c5dc7957d02069db7e41190b3a8d4f
Julian Lamadrid <jelamadrid@e-din.com.ar>
2013-10-07 21:25:16 -0300

    No se necesitaron mas:wq

--- app/models/driver_license_code.rb
+++
@@ -1,7 +0,0 @@
-class DriverLicenseCode < ActiveRecord::Base
-  attr_accessible :code, :description
-  has_many :driver_licenses
-  has_many :employees, through: :driver_licenses
-
-  accepts_nested_attributes_for :driver_licenses
-end
